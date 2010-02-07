0a97e67d09908c90affe06d7045d2d6fa26b556a
Samuel Abels <knipknap@gmail.com>
2010-02-07 16:43:22 +0100

    Improved behavior when used in a threaded environment.

--- src/Gelatin/util.py
+++ src/Gelatin/util.py
@@ -2,26 +2,11 @@ import generator
 from parser   import Parser
 from compiler import SyntaxCompiler
 
-_parser   = None
-_compiler = None
-
-def get_parser():
-    global _parser
-    if not _parser:
-        _parser = Parser()
-    return _parser
-
-def get_compiler():
-    global _compiler
-    if not _compiler:
-        _compiler = SyntaxCompiler()
-    return _compiler
-
 def compile_string(syntax):
-    return get_parser().parse_string(syntax, get_compiler())
+    return Parser().parse_string(syntax, SyntaxCompiler())
 
 def compile(syntax_file):
-    return get_parser().parse(syntax_file, get_compiler())
+    return Parser().parse(syntax_file, SyntaxCompiler())
 
 def generate_to_file(converter, input_file, output_file, format = 'xml'):
     builder = generator.new(format)
