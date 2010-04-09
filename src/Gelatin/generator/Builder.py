274ad6f837341ff0784fdb59bf809e3221f3acda
Samuel Abels <knipknap@gmail.com>
2010-04-09 08:41:07 +0200

    Fix: convert XML attribute names to strings to avoid unicode errors.

--- src/Gelatin/generator/Builder.py
+++ src/Gelatin/generator/Builder.py
@@ -59,7 +59,7 @@ class Builder(object):
             value = value[0]
             if value.startswith('"') and value.endswith('"'):
                 value = value[1:-1]
-            attribs.append((key.lower(), value))
+            attribs.append((str(key.lower()), value))
         return url.path.replace(' ', '-').lower(), attribs
 
     def create(self, path, data = None):
