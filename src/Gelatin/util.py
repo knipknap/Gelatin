d3909b5d315d2d6b308b78c36aa2dd17810a89f6
Samuel Abels <knipknap@gmail.com>
2010-06-29 13:04:23 +0200

    Add util.generate_string_to_file().

--- src/Gelatin/util.py
+++ src/Gelatin/util.py
@@ -14,3 +14,10 @@ def generate_to_file(converter, input_file, output_file, format = 'xml'):
         raise TypeError('invalid output format ' + repr(format))
     converter.parse(input_file, builder)
     builder.serialize_to_file(output_file)
+
+def generate_string_to_file(converter, input, output_file, format = 'xml'):
+    builder = generator.new(format)
+    if builder is None:
+        raise TypeError('invalid output format ' + repr(format))
+    converter.parse_string(input, builder)
+    builder.serialize_to_file(output_file)
